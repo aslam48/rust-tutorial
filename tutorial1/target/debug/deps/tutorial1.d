898a03778fea7e9469d653bbaac077ce0c173e21
aslam48 <youngfizzy001@gmail.com>
2023-10-07 11:31:44 +0100

    learning

---
+++ tutorial1/target/debug/deps/tutorial1.d
@@ -0,0 +1,5 @@
+C:\Users\ASLAM ABD SOBUR\Desktop\rust-tutorial\tutorial1\target\debug\deps\tutorial1.exe: src\main.rs
+
+C:\Users\ASLAM ABD SOBUR\Desktop\rust-tutorial\tutorial1\target\debug\deps\tutorial1.d: src\main.rs
+
+src\main.rs:
---
+++ tutorial1/target/debug/tutorial1.d
@@ -0,0 +1,1 @@
+C:\Users\ASLAM\ ABD\ SOBUR\Desktop\rust-tutorial\tutorial1\target\debug\tutorial1.exe: C:\Users\ASLAM\ ABD\ SOBUR\Desktop\rust-tutorial\tutorial1\src\main.rs
