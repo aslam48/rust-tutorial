cd405bdc8d25e735f45ebf5c715cf3ec02329656
aslam48 <youngfizzy001@gmail.com>
2023-10-23 11:16:34 +0100

    learning

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -12,12 +12,39 @@ fn test_aslam() {
     // let goat = false;
     println!("{}", goat);
 
-
     let ages: [u16; 6] = [34, 40, 55, 20, 12, 32];
     println!("{:?}", ages);
 
     let newage: &[u16] = &ages[1..4];
     println!("{:?}", newage);
 
-    
+    let tup: (&str, &str, u8) = ("okey", "hi", 40);
+    println!("{:?}", tup.2);
+
+    // condictional statment 
+    let number: i32 = 10;
+    if number > 11 {
+        print!("condiction was true")
+    } else {
+        print!("condiction was not true")
+    }
+
+    //   loop 
+    let mut counter: i32 = 0;
+    let result: i32 = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter;
+        }
+    };
+    println!("The result is {}", result);
+
+
+    // while loop 
+    let mut number = 1000;
+    while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+    }
+    println!("LIFTOFF!!!");
 }
--- data_types/target/debug/deps/dataTypes-66d4e5843b5d8d6f.d
+++ data_types/target/debug/deps/dataTypes-66d4e5843b5d8d6f.d
@@ -1,5 +1,5 @@
-C:\Users\Aslam iliyas\Desktop\rust-tutorial\dataTypes\target\debug\deps\dataTypes-66d4e5843b5d8d6f.rmeta: src\main.rs
+C:\Users\Aslam iliyas\Desktop\rust-tutorial\data_types\target\debug\deps\dataTypes-66d4e5843b5d8d6f.rmeta: src\main.rs
 
-C:\Users\Aslam iliyas\Desktop\rust-tutorial\dataTypes\target\debug\deps\dataTypes-66d4e5843b5d8d6f.d: src\main.rs
+C:\Users\Aslam iliyas\Desktop\rust-tutorial\data_types\target\debug\deps\dataTypes-66d4e5843b5d8d6f.d: src\main.rs
 
 src\main.rs:
