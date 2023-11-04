16473a83a9a1292e0f846b30bc3c6a9710c5bf10
aslam48 <youngfizzy001@gmail.com>
2023-11-04 17:26:21 +0100

    working

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 
 fn main() {
@@ -10,4 +11,9 @@ fn main() {
  .expect("Failed to read line");
  println!("You guessed: {}", guess);
  println!("You guessed: {}", guess);
+
+
+ let secret_number = rand::thread_rng().gen_range(1, 101);
+ println!("The secret number is: {}", secret_number);
 }
+
