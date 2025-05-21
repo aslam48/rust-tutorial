0860f56b302e379f87c0bd49c68104195ca978f7
aslam48 <youngfizzy001@gmail.com>
2025-05-21 15:57:39 +0100

    working on emums

---
+++ enum-tutotial/src/main.rs
@@ -0,0 +1,37 @@
+// enum IpKindAddress {
+//     V4,
+//     V6,
+// }
+
+// struct IpKind {
+//     kind: IpKindAddress,
+//     address: String
+// }
+
+// fn main() {
+//   let four = IpKindAddress::V4
+//   let six = IpKindAddress::V6
+
+//   let home IpKind = {
+//     kind:IpKindAddress::V4
+//     address: string::from("123::34::56")
+//   }
+
+// }
+
+
+
+
+
+
+
+// fn main() {
+//     enum IpAddr {
+//         V4(u8, u8, u8, u8),
+//         V6(String),
+//     }
+
+//     let home = IpAddr::V4(127, 0, 0, 1);
+
+//     let loopback = IpAddr::V6(String::from("::1"));
+// }
